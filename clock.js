2b2035306b64a22cd70d05e8b0a9531464795452
vipin <xmartcoder@gmail.com>
2023-01-03 04:51:28 +0530

    responsive

--- clock.js
+++ clock.js
@@ -2,16 +2,15 @@ var secondpin = document.querySelector('#second');
 var minutepin = document.querySelector('#minute');
 var hourpin = document.querySelector('#hour');
 
-
 var date = new Date();
     var second = date.getSeconds();
     var minuteins =date.getMinutes()*60+second;
     var hourins =date.getHours()*60*60+date.getMinutes()*60+second;
     let s=second,m=minuteins,h=hourins;
-    const sound = new Audio('watch.mp3');
+    const sound = new Audio('clock.mp3');
 setInterval(() => {
     sound.play();
-    secondpin.style = `transform: translateY(-10vw) rotate(${s++ * 6 + 180}deg)`;
-    minutepin.style = `transform:translateY(2.5vw) rotate(${m++*0.1 + 180}deg)`;
-    hourpin.style = `transform:translateY(2.5vw) rotate(${h++*(1/120) + 180}deg)`;
+    secondpin.style = `transform: translateY(25%) rotate(${s++ * 6 + 180}deg)`;
+    minutepin.style = `transform:translateY(50%) rotate(${m++*0.1 + 180}deg)`;
+    hourpin.style = `transform:translateY(50%) rotate(${h++*(1/120) + 180}deg)`;
 }, 1000);
